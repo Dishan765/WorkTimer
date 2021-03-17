e7b8262d22b1e521f20d6d99a0325d82019d0f8a
dishan <you@example.com>
2021-03-17 16:00:20 +0400

    changed db server name

--- worktimer/config.py
+++ worktimer/config.py
@@ -5,12 +5,12 @@ username = "dishan765"
 password = "python12345"
 #server = "localhost"
 server = "dishan765.mysql.pythonanywhere-services.com"
-database = "WorkTimer"
+#database = "WorkTimer"
+database ="dishan765$WorkTimer"
 
 class Config:
   #SEND_FILE_MAX_AGE_DEFAULT = 0#to remove
   #SECRET_KEY = 'l\x16\xc6\xd7\xc1\xd3\x17\xc9\xc0\xd1W\xa9kH\x01\xf0'
   #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' 
-  #SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@{server}/{database}"
-  SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{server}/{databasename}"
+  SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@{server}/{database}"
   SQLALCHEMY_TRACK_MODIFICATION = False
