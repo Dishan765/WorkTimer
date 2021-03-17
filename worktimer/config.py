ce2caa807bbc55f02a46d22d1293c878c135e949
dishan <you@example.com>
2021-03-17 15:51:10 +0400

    changed db server name

--- worktimer/config.py
+++ worktimer/config.py
@@ -12,5 +12,5 @@ class Config:
   #SECRET_KEY = 'l\x16\xc6\xd7\xc1\xd3\x17\xc9\xc0\xd1W\xa9kH\x01\xf0'
   #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db' 
   #SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{username}:{password}@{server}/{database}"
-  SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}"
+  SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{server}/{databasename}"
   SQLALCHEMY_TRACK_MODIFICATION = False
